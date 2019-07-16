e5598fe9b216b0c4a67691a098cea94321590600
Wagner Bernardes Teixeira <wagnerbernardesteixeira@gmail.com>
2019-07-16 18:57:58 -0300

    Initial conversion mongo to mysql

---
+++ conversions/conversion-mongo-mysql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace conversion_mongo_mysql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
