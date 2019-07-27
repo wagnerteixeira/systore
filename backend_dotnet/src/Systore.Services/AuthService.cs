41fe4f5398ae72d18d00c5607808ce4cbc09ab29
Wagner Bernardes Teixeira <wagnerbernardesteixeira@gmail.com>
2019-07-27 00:12:13 -0300

    init authService

---
+++ backend_dotnet/src/Systore.Services/AuthService.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Systore.Domain.Abstractions;
+using Systore.Domain.Entities;
+using Systore.Data.Abstractions;
+using System.Linq.Expressions;
+
+
+namespace Systore.Services
+{
+    public class AuthService
+    {
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto){
+          
+        }
+    }
+}
