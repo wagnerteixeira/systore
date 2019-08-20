c0ef9330a227071b52856c54f4a1aad00429e385
Wagner Bernardes Teixeira <wagnerbernardesteixeira@gmail.com>
2019-08-19 23:12:21 -0300

    init balance load

--- backend_dotnet/src/Systore.Services/ProductService.cs
+++ backend_dotnet/src/Systore.Services/ProductService.cs
@@ -1,14 +1,23 @@
 ﻿using Systore.Domain.Abstractions;
 using Systore.Domain.Entities;
 using Systore.Data.Abstractions;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Systore.Domain.Dtos;
 
 namespace Systore.Services
 {
     public class ProductService : BaseService<Product>, IProductService
     {
+
         public ProductService(IProductRepository repository) : base(repository)
         {
 
+        }
+
+        public Task<List<Product>> GetProductsForExportToBalance(FilterProductsToBalance filterProductsToBalance)
+        {
+            return (_repository as IProductRepository).GetProductsForExportToBalance(filterProductsToBalance);
         }
     }
 }
