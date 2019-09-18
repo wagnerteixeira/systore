912d1136aba0725a232d5e3d41f03d35811b1a4f
Sanye Caroline <sanyecg@gmail.com>
2019-09-17 21:30:27 -0300

    add mother and father in report

--- frontend/src/pages/reports/PrintDefaulters.js
+++ frontend/src/pages/reports/PrintDefaulters.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import MessageSnackbar from '../../components/common/MessageSnackbar';
 import { Grid, Button, Paper } from '@material-ui/core';
