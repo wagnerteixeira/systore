ad3d7ac3e0cbf5e5feb9b09593d492f4babadfee
Sanye Caroline <sanyecg@gmail.com>
2019-10-02 21:59:47 -0300

    correction convert cpf string

---
+++ conversions/conversion-mongo-mysql/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace conversion_mongo_mysql
+{
+    public static class Utils
+    {
+
+        public static string OnlyNumbers(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return null;
+            Regex regexObj = new Regex(@"[^\d]");
+            return regexObj.Replace(value, "");
+        }
+    }
+}
