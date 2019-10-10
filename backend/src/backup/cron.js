833ad8d87c2d1e1fac4dced1a7a091462b572fbd
Wagner Bernardes Teixeira <wagnerbernardesteixeira@gmail.com>
2019-10-10 00:31:09 -0300

    modify time of backup

--- backend/src/backup/cron.js
+++ backend/src/backup/cron.js
@@ -3,7 +3,17 @@ var Cron = require("./backupTask");
 console.log(Cron.backupTask);
 
 exports.default = new CronJob({
-  cronTime: "0 28 00 * * *",
+  cronTime: "0 30 19 * * *",
+  onTick: function() {
+    Cron.backupTask();
+  },
+  start: true,
+  onComplete: () => console.log("Complete backup"),
+  timeZone: "America/Sao_Paulo"
+});
+
+new CronJob({
+  cronTime: "0 0 13 * * *",
   onTick: function() {
     Cron.backupTask();
   },
